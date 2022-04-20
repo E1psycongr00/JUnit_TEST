9df0f1dbc9ef917fbb010a92deb7490bcfe5bf4f
E1psycongr00 <marrin1101@gmail.com>
2022-04-20 17:37:19 +0900

    Feat: Tag annotation 사용법 추가

---
+++ src/test/java/com/example/junit_test/tag/TagAnnotationTest.java
@@ -0,0 +1,36 @@
+package com.example.junit_test.tag;
+
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/*
+    테스트 클래스, 메소드에 테스트 구분을 태깅하기 위해 사용
+ */
+
+@Tag("fast")
+public class TagAnnotationTest {
+
+    @Test
+    @Tag("fast")
+    void fast_1() {
+
+    }
+    @Test
+    @Tag("fast")
+    void fast_2() {
+
+    }
+
+    @Test
+    @Tag("slow")
+    void slow_1() {
+
+    }
+
+    @Test
+    @Tag("slow")
+    void slow_2() {
+
+    }
+}
