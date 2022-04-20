26eca6769bd502f983e58d7eac1368bff79dbf35
E1psycongr00 <marrin1101@gmail.com>
2022-04-20 16:59:50 +0900

    Fix: ExtendWith lifeCycle 설명 추가

--- src/test/java/extendwith/TimingExtensionTest.java
+++ src/test/java/extendwith/TimingExtensionTest.java
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+/*
+    1. lifeCycle 콜백 함수를 이용한 구현체를 Extendwith에 넣어주면 test에 AOP처럼 사용 할 수 있음.
+    2. SpringExtension.class를 넣어주는 경우 SpringContext환경을 제공해준다.
+        - bean을 활용하기 가능(bean은 따로 주입해줘야 한다.)
+        - ApplicationContext 가 하나 만들어진다고 보면 된다.
+ */
 @ExtendWith(TimingExtension.class)
 public class TimingExtensionTest {
 
